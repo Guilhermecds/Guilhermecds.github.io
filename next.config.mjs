2013b52552801f4b3125450cfce712b8e056022a
Guilhermecds <guilherme.gazinlab@gmail.com>
2024-05-24 01:56:36 -0300

    adicionado regra para mandar todo http para https

--- next.config.mjs
+++ next.config.mjs
@@ -1,41 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/guilhermecds.github.io',
-  assetPrefix: '/guilhermecds.github.io/',
-  trailingSlash: true,
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/',
-        permanent: true,
-        basePath: false,
-        statusCode: 301,
-        has: [
-          {
-            type: 'host',
-            value: 'guilhermecds.github.io',
-          },
-        ],
-        href: '/',
-      },
-      {
-        source: 'http://guilhermecds.github.io/:path*',
-        destination: 'https://guilhermecds.github.io/:path*',
-        permanent: true,
-        basePath: false,
-        statusCode: 301,
-        has: [
-          {
-            type: 'host',
-            value: 'guilhermecds.github.io',
-          },
-        ],
-        href: 'https://guilhermecds.github.io/:path*',
-      },
-    ];
-  },
+    output: 'export',
+    basePath: '/guilhermecds.github.io',
+    assetPrefix: '/guilhermecds.github.io/',
+    trailingSlash: true,
 };
   
-export default nextConfig;
+  export default nextConfig;

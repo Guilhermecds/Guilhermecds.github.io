f8240a4a1a2b2214bb5b8282bb8535f325ce0dcd
Guilhermecds <guilherme.gazinlab@gmail.com>
2024-05-24 01:07:35 -0300

    adicionado deploy do next

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export',
+    basePath: '/guilhermecds.github.io',
+    assetPrefix: '/guilhermecds.github.io/',
+    trailingSlash: true,
+};
+  
+  export default nextConfig;

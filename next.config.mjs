07ec0a8a272470312b7de99e3255897edb8f39ee
Guilhermecds <guilherme.gazinlab@gmail.com>
2024-05-24 01:51:38 -0300

    adicionado configuração para https

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,41 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    basePath: '/guilhermecds.github.io',
-    assetPrefix: '/guilhermecds.github.io/',
-    trailingSlash: true,
+  output: 'export',
+  basePath: '/guilhermecds.github.io',
+  assetPrefix: '/guilhermecds.github.io/',
+  trailingSlash: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/',
+        permanent: true,
+        basePath: false,
+        statusCode: 301,
+        has: [
+          {
+            type: 'host',
+            value: 'guilhermecds.github.io',
+          },
+        ],
+        href: '/',
+      },
+      {
+        source: 'http://guilhermecds.github.io/:path*',
+        destination: 'https://guilhermecds.github.io/:path*',
+        permanent: true,
+        basePath: false,
+        statusCode: 301,
+        has: [
+          {
+            type: 'host',
+            value: 'guilhermecds.github.io',
+          },
+        ],
+        href: 'https://guilhermecds.github.io/:path*',
+      },
+    ];
+  },
 };
   
-  export default nextConfig;
+export default nextConfig;
